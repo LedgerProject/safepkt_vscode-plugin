c8fcbda5f87c42515d1efb42c1b1b6cd1e330aaa
Thierry Marianne <thierry@marianne.io>
2021-10-20 18:02:15 +0400

    Start working on smart-contract verifier extension for vscode

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,18 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as cp from "child_process";
+
+const execShell = (cmd: string) =>
+    new Promise<string>((resolve, reject) => {
+		console.log({cmd});
+        cp.exec(cmd, (err, out) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(out);
+        });
+    });
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,10 +25,22 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('safepkt-verifier.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from SafePKT verifier!');
+	let disposable = vscode.commands.registerCommand('safepkt-verifier.helloWorld', async () => {
+		try {
+			const output  = await execShell('/home/sh4l/repositories/pkt/safepkt-backend/target/release/safepkt-cli verify_program --source ./examples/erc721.rs');
+
+			// The code you place here will be executed every time your command is executed
+			// Display a message box to the user
+			vscode.window.showInformationMessage(output);
+		} catch (e) {
+			if (!(e instanceof Error)) {
+				console.error(e);
+				return;
+			}
+
+			console.debug(e);
+			vscode.window.showInformationMessage(e.message);
+		}
 	});
 
 	context.subscriptions.push(disposable);
