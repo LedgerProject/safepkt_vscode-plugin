5efbe4f2b0839867cc5ff65dac33e269caf3b49f
Thierry Marianne <thierry@marianne.io>
2021-10-21 18:05:40 +0400

    Implement verification task

--- src/extension.ts
+++ src/extension.ts
@@ -1,50 +1,28 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as cp from "child_process";
+import { SafePKTSmartContrackVerifier } from './safePKTSmartContrackVerifier';
 
-const execShell = (cmd: string) =>
-    new Promise<string>((resolve, reject) => {
-		console.log({cmd});
-        cp.exec(cmd, (err, out) => {
-            if (err) {
-                return reject(err);
-            }
-            return resolve(out);
-        });
-    });
+let safePKTSmartContrackVerifier: vscode.Disposable | undefined;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "safepkt-verifier" is now active!');
+	const workspaceRoot = (vscode.workspace.workspaceFolders && (vscode.workspace.workspaceFolders.length > 0))
+		? vscode.workspace.workspaceFolders[0].uri.fsPath : undefined;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('safepkt-verifier.helloWorld', async () => {
-		try {
-			const output  = await execShell('/home/sh4l/repositories/pkt/safepkt-backend/target/release/safepkt-cli verify_program --source ./examples/erc721.rs');
+		if (!workspaceRoot) {
+		return;
+	}
 
-			// The code you place here will be executed every time your command is executed
-			// Display a message box to the user
-			vscode.window.showInformationMessage(output);
-		} catch (e) {
-			if (!(e instanceof Error)) {
-				console.error(e);
-				return;
-			}
-
-			console.debug(e);
-			vscode.window.showInformationMessage(e.message);
-		}
-	});
-
-	context.subscriptions.push(disposable);
+	safePKTSmartContrackVerifier = vscode.tasks.registerTaskProvider(
+		SafePKTSmartContrackVerifier.smartContractVerificationType,
+		new SafePKTSmartContrackVerifier(workspaceRoot)
+	);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate(): void {
+	if (safePKTSmartContrackVerifier) {
+		safePKTSmartContrackVerifier.dispose();
+	}
+}
---
+++ src/safePKTSmartContrackVerifier.ts
@@ -0,0 +1,251 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) CJDNS SASU. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import * as fs from 'fs';
+import * as path from 'path';
+import * as vscode from 'vscode';
+import * as cp from 'child_process';
+
+function exec(command: string, options: cp.ExecOptions): Promise<{ stdout: string; stderr: string }> {
+	return new Promise<{ stdout: string; stderr: string }>((resolve, reject) => {
+		cp.exec(command, options, (error, stdout, stderr) => {
+			if (error) {
+				reject({ error, stdout, stderr });
+			}
+
+            resolve({ stdout, stderr });
+		});
+	});
+}
+
+function exists(file: string): Promise<boolean> {
+	return new Promise<boolean>((resolve, _reject) => {
+		fs.exists(file, (value) => {
+			resolve(value);
+		});
+	});
+}
+
+
+async function getSafePKTSmartContractVerifierTasks({state: sharedState}: {state: string | undefined}): Promise<vscode.Task[]> {
+    const workspaceFolders = vscode.workspace.workspaceFolders;
+    const result: vscode.Task[] = [];
+
+    if (!workspaceFolders || workspaceFolders.length === 0) {
+        return result;
+    }
+
+    for (const workspaceFolder of workspaceFolders) {
+        const folderString = workspaceFolder.uri.fsPath;
+        if (!folderString) {
+            continue;
+        }
+
+        const commandLine = path.join(folderString, '/scripts/safepkt-cli');
+        if (!await exists(commandLine)) {
+            continue;
+        }
+
+        try {
+            const { stdout, stderr } = await exec(`${commandLine} verify_program --help`, { cwd: folderString });
+
+            if (stderr && stderr.length > 0) {
+                getOutputChannel().appendLine(stderr);
+                getOutputChannel().show(true);
+            }
+
+            if (stdout) {
+                const lines = stdout.split(/\n/);
+                for (const line of lines) {
+                    if (line.length === 0) {
+                        continue;
+                    }
+                                    
+                    const definition = {
+                        type: SafePKTSmartContrackVerifier.smartContractVerificationType,
+                        smartContractPath: "/tmp/374567ab67/src/lib.rs"
+                    };
+
+                    const task = new vscode.Task(
+                        definition,
+                        vscode.TaskScope.Workspace,
+                        `Verify Smart Contract available at "${definition.smartContractPath}"`,
+                        SafePKTSmartContrackVerifier.smartContractVerificationType,
+                        new vscode.CustomExecution(async (): Promise<vscode.Pseudoterminal> => {
+                            // When the task is executed, this callback will run. Here, we setup for running the task.
+                            return new SafePKTSmartContractVerificationTaskTerminal(
+                                folderString, 
+                                definition.smartContractPath,
+                                () => sharedState,
+                                (state: string) => sharedState = state
+                            );
+                        }));
+
+                    result.push(task);
+
+                    task.group = vscode.TaskGroup.Test;
+                }
+            }
+        } catch (err: any) {
+            const channel = getOutputChannel();
+
+            if (err.stderr) {
+                channel.appendLine(err.stderr);
+            }
+
+            if (err.stdout) {
+                channel.appendLine(err.stdout);
+            }
+
+            channel.appendLine('Auto detecting safepkt-cli tasks failed.');
+            channel.show(true);
+        }
+    };
+
+    return result;
+}
+
+interface SafePKTSmartContractVerificationTaskDefinition extends vscode.TaskDefinition {
+	smartContractPath: string;
+}
+
+export class SafePKTSmartContrackVerifier implements vscode.TaskProvider {
+	static smartContractVerificationType = 'safePKTSmartContractVerification';
+
+    private tasks: vscode.Task[] | undefined;
+
+    private safePKTVerifierPromise: Thenable<vscode.Task[]> | undefined = undefined;
+
+	// We use a CustomExecution task when state needs to be shared across runs of the task or when 
+	// the task requires use of some VS Code API to run.
+	// If you don't need to share state between runs and if you don't need to execute VS Code API in your task, 
+	// then a simple ShellExecution or ProcessExecution should be enough.
+	// Since our build has this shared state, the CustomExecution is used below.
+	private sharedState: string | undefined;
+
+	constructor(private workspaceRoot: string) { }
+
+	// public async provideTasks(): Promise<vscode.Task[]> {
+    //     const state = {state: this.sharedState};
+
+	// 	return getSafePKTSmartContractVerifierTasks(state);
+    // }
+
+	public async provideTasks(): Promise<vscode.Task[]> {
+		return this.getTasks();
+	}
+
+	public resolveTask(_task: vscode.Task): vscode.Task | undefined {
+		const smartContractPath: string = _task.definition.smartContractPath;
+
+        if (smartContractPath) {
+			const definition: SafePKTSmartContractVerificationTaskDefinition = <any>_task.definition;
+			return this.getTask(definition.smartContractPath, definition);
+		}
+
+        return undefined;
+	}
+
+	private getTasks(): vscode.Task[] {
+		if (this.tasks !== undefined) {
+			return this.tasks;
+		}
+
+		const smartContractPath: string = '/tmp/374567ab67/src/lib.rs';
+
+		this.tasks = [];
+        this.tasks!.push(this.getTask(smartContractPath));
+
+        return this.tasks;
+	}
+
+	private getTask(
+        smartContractPath: string,
+        definition?: SafePKTSmartContractVerificationTaskDefinition
+    ): vscode.Task {
+		if (definition === undefined) {
+			definition = {
+				type: SafePKTSmartContrackVerifier.smartContractVerificationType,
+				smartContractPath,
+			};
+		}
+
+        return new vscode.Task(
+            definition,
+            vscode.TaskScope.Workspace,
+            `Verify Smart Contract available at "${smartContractPath}"`,
+			SafePKTSmartContrackVerifier.smartContractVerificationType,
+            new vscode.CustomExecution(async (): Promise<vscode.Pseudoterminal> => {
+				// When the task is executed, this callback will run. Here, we setup for running the task.
+				return new SafePKTSmartContractVerificationTaskTerminal(
+                    this.workspaceRoot,
+                    smartContractPath,
+                    () => this.sharedState, (state: string) => this.sharedState = state
+                );
+			}));
+	}
+}
+
+let _channel: vscode.OutputChannel;
+function getOutputChannel(): vscode.OutputChannel {
+	if (!_channel) {
+		_channel = vscode.window.createOutputChannel('safepkt-cli auto detection');
+	}
+	return _channel;
+}
+
+class SafePKTSmartContractVerificationTaskTerminal implements vscode.Pseudoterminal {
+
+    private writeEmitter = new vscode.EventEmitter<string>();
+	onDidWrite: vscode.Event<string> = this.writeEmitter.event;
+
+    private closeEmitter = new vscode.EventEmitter<number>();
+	onDidClose?: vscode.Event<number> = this.closeEmitter.event;
+
+	private fileWatcher: vscode.FileSystemWatcher | undefined;
+
+	constructor(
+        private workspaceRoot: string,
+        private smartContractPath: string,
+        private getSharedState: () => string | undefined,
+        private setSharedState: (state: string) => void
+    ) {
+	}
+
+	open(initialDimensions: vscode.TerminalDimensions | undefined): void {
+		// At this point we can start using the terminal.
+        const pattern = path.join(this.workspaceRoot, 'src/*.rs');
+
+        console.log(pattern);
+
+        const fileWatcher = vscode.workspace.createFileSystemWatcher(pattern);
+
+        fileWatcher.onDidChange(() => this.doVerify());
+		fileWatcher.onDidCreate(() => this.doVerify());
+		fileWatcher.onDidDelete(() => this.doVerify());
+
+        this.doVerify();
+	}
+
+    private async doVerify(): Promise<void> {
+		return new Promise<void>((resolve) => {
+            // const { stdout, stderr } = await exec(`${commandLine} verify_program --help`, { cwd: folderString });
+
+            this.writeEmitter.fire(`${this.workspaceRoot}/src/lib.rs`);
+
+            this.writeEmitter.fire('Starting rust-based smart contract verification.\r\n');
+            this.writeEmitter.fire('Rust smart contract verification complete.\r\n\r\n');
+            this.closeEmitter.fire(0);
+
+            resolve();
+		});
+	}
+
+	close(): void {
+		// The terminal has been closed. Shutdown the build.
+		if (this.fileWatcher) {
+			this.fileWatcher.dispose();
+		}
+	}
+}
